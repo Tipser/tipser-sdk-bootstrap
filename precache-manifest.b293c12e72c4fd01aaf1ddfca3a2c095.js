aeb666abf76d3dec58ae85638f4adb5963f25b75
Wojciech Piątkowski <chmurson@gmail.com>
2019-05-14 11:52:57 +0200

    Updates

---
+++ precache-manifest.b293c12e72c4fd01aaf1ddfca3a2c095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1520c4f80bb0c7f850f45798b05484a",
+    "url": "/tipser-sdk-bootstrap/index.html"
+  },
+  {
+    "revision": "a260918ba8b11c790a8f",
+    "url": "/tipser-sdk-bootstrap/static/css/main.bdb35151.chunk.css"
+  },
+  {
+    "revision": "fdb39b885d7d0732f458",
+    "url": "/tipser-sdk-bootstrap/static/js/2.c70b8ffc.chunk.js"
+  },
+  {
+    "revision": "a260918ba8b11c790a8f",
+    "url": "/tipser-sdk-bootstrap/static/js/main.d4088858.chunk.js"
+  },
+  {
+    "revision": "56cd6a1f1c5aaad4840a",
+    "url": "/tipser-sdk-bootstrap/static/js/runtime~main.a0ade12f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tipser-sdk-bootstrap/static/media/logo.5d5d9eef.svg"
+  }
+]);
