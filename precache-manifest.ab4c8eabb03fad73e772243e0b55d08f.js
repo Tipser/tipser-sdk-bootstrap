e0f7a448b4e9bd36e3c0297eec6cbad2f33aefd8
Wojciech Piątkowski <chmurson@gmail.com>
2019-12-17 13:20:50 +0100

    Updates

--- precache-manifest.ab4c8eabb03fad73e772243e0b55d08f.js
+++ precache-manifest.ab4c8eabb03fad73e772243e0b55d08f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68d8dc8d38510e560a6c0b78df8f74ee",
+    "revision": "9285e09e020536952062196dceea4f8a",
     "url": "/tipser-sdk-bootstrap/index.html"
   },
   {
-    "revision": "23024d640399b44af63c",
+    "revision": "615b34fd0c9f6b4295e4",
     "url": "/tipser-sdk-bootstrap/static/css/main.f55ac6ae.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tipser-sdk-bootstrap/static/js/2.0633bbb7.chunk.js"
   },
   {
-    "revision": "23024d640399b44af63c",
-    "url": "/tipser-sdk-bootstrap/static/js/main.d57110e4.chunk.js"
+    "revision": "615b34fd0c9f6b4295e4",
+    "url": "/tipser-sdk-bootstrap/static/js/main.25ce8725.chunk.js"
   },
   {
     "revision": "56cd6a1f1c5aaad4840a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tipser-sdk-bootstrap/precache-manifest.018af82229f205b28f0db8758ec8e2da.js"
+  "/tipser-sdk-bootstrap/precache-manifest.ab4c8eabb03fad73e772243e0b55d08f.js"
 );
 
 self.addEventListener('message', (event) => {
